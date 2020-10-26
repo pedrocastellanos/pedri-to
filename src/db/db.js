4b3786a82d69387533c3acdb3719fef9f45db8f6
Your Name <pedro.castellanos2003@gmail.com@gmail.com>
2020-10-26 17:08:42 -0400

    7 mensaje

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/urlshortener', {
+
+const uri = 'mongodb+srv://pedrito:<password>@cluster0.usq6d.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
+
+mongoose.connect(uri, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
 })
+
     .then(db=>console.log('DB is connected'))
     .catch(err=> console.log(err))
