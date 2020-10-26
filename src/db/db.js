20b6bdd0307b39e6416d35ff371f713bfa04c127
Your Name <pedro.castellanos2003@gmail.com@gmail.com>
2020-10-26 17:31:30 -0400

    asdasd

--- src/db/db.js
+++ src/db/db.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose');
 const uri = 'mongodb+srv://pedrito:<Osaris03121971280>@cluster0.usq6d.mongodb.net/<urlshortenerdb>?retryWrites=true&w=majority'
 
 
-mongoose.connect(uri, {
-    // useCreateIndex: true,
-    // useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+try {
+    mongoose.connect( uri, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+    console.log("connected"));    
+    }catch (error) { 
+    console.log("could not connect", error);    
+    }
 
-    .then(db=>console.log('DB is connected'))
-    .catch(err=> console.log(err))
+    // .then(db=>console.log('DB is connected'))
+    // .catch(err=> console.log(err))
