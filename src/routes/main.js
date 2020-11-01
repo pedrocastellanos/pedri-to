aede1a06209a75377a0b3eaa643fe2f35a20234e
Your Name <pedro.castellanos2003@gmail.com@gmail.com>
2020-11-01 11:41:45 -0500

    pedrito

--- src/routes/main.js
+++ src/routes/main.js
@@ -58,9 +58,12 @@ router.post('/register', async (req, res)=>{
         res.render('register', {errors, name, email, password, confirm_password})
     } else {
         console.log('Was Here', 'line 60')
-        const emailUser = await User.findOne({email: email}, (err)=>{
-            if (err) return console.error(err)
-        })
+        // const emailUser = await User.findOne({email: email}, (err)=>{
+        //     if (err) return console.error(err)
+        // })
+        const emailUser = await User.findOne({ email: email }, function (err) {
+            if (err) return console.error(err);
+        });
         if (emailUser) {
             req.flash('success_msg', 'This email already in use')
             console.log(req.flash())
